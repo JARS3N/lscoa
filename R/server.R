f0709f6c9c08c3bb960274ba0bc9d855d43b95a1
JARS3N <JARS3N@users.noreply.github.com>
2023-12-13 11:02:40 -0500

    Update server.R

--- R/server.R
+++ R/server.R
@@ -1,43 +1,26 @@
-server <- function() {
-  ######
-  library(shiny)
-  library(dplyr)
-  library(tesseract)
-  print("start")
-  message('start message')
-  function(input, output, session) {
-    # data <- reactive({
-    #   req(input$upload)
-    #   #munge_CoA(input$upload$datapath)
-    #   # DEBUGGING *
-    #   u<-input$upload$datapath
-    #   message('txt portion')
-    #   eng<-tesseract("eng")
-    #   txt <- tesseract::ocr(u,engine=eng)
-    #   cat(txt)
-    #   message('txt_vec')
-    #   txt_vec<-strsplit(txt[[1]],split="\n")[[1]]
-    #   message('summarize')
-    #   summarize_CoA(txt_vec)
-    #   #*****
-    # })
-      output$head <- renderTable({})
-    observeEvent(input$upload,{
-      u<-input$upload$datapath
-      message('txt portion')
-      eng<-tesseract("eng")
-      txt <- tesseract::ocr(u,engine=eng)
-      cat(txt)
-      message('txt_vec')
-      txt_vec<-strsplit(txt[[1]],split="\n")[[1]]
-      message('summarize')
-      TBL<-summarize_CoA(txt_vec)
-      print(TBL)
-    })
-    
-    
-    output$head <- renderTable({TBL})
-    
-  }
-  ###
+server<-function(u){
+
+    library(shiny)
+    library(tesseract)
+    library(dplyr)
+    function(input, output, session) {
+      observeEvent(input$upload, {
+        u <- input$upload$datapath
+        message('txt portion')
+        eng <- tesseract("eng")
+        txt <- tesseract::ocr(u, engine = eng)
+        txt_vec <- strsplit(txt[[1]], split = "\n")[[1]]
+        output$text <- renderText({
+          txt_vec
+        })
+        q <-  lscoa::summarize_CoA(txt_vec)
+        output$head <- renderTable({
+          q
+        })
+      })
+      
+      
+    }
+
+
 }
