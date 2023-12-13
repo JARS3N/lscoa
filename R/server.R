77b126e98fedcfdcdfbe9cf24ff7e414c6ba713f
JARS3N <JARS3N@users.noreply.github.com>
2023-12-13 09:21:50 -0500

    Update server.R

--- R/server.R
+++ R/server.R
@@ -9,16 +9,17 @@ server <- function() {
     data <- reactive({
       req(input$upload)
       #munge_CoA(input$upload$datapath)
-      # DEBUGGING
+      # DEBUGGING *
       u<-input$upload$datapath
       message('txt portion')
       eng<-tesseract("eng")
       txt <- tesseract::ocr(u,engine=eng)
+      cat(txt)
       message('txt_vec')
       txt_vec<-strsplit(txt[[1]],split="\n")[[1]]
       message('summarize')
       summarize_CoA(txt_vec)
-      #
+      #*****
     })
 
     output$head <- renderTable({
